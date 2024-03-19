85264aaf5fcc3402d66e0bf2c18b948975e59748
Noah Hazan <72990446+noah-hazan-repo@users.noreply.github.com>
2024-03-19 18:42:01 +0000

    snowpark_tutorial

--- steps/01_setup_snowflake.sql
+++ steps/01_setup_snowflake.sql
@@ -1,40 +1,40 @@
-/*-----------------------------------------------------------------------------
-Hands-On Lab: Data Engineering with Snowpark
-Script:       01_setup_snowflake.sql
-Author:       Jeremiah Hansen
-Last Updated: 1/1/2023
------------------------------------------------------------------------------*/
+-- /*-----------------------------------------------------------------------------
+-- Hands-On Lab: Data Engineering with Snowpark
+-- Script:       01_setup_snowflake.sql
+-- Author:       Jeremiah Hansen
+-- Last Updated: 1/1/2023
+-- -----------------------------------------------------------------------------*/
 
 
--- ----------------------------------------------------------------------------
--- Step #1: Accept Anaconda Terms & Conditions
--- ----------------------------------------------------------------------------
+-- -- ----------------------------------------------------------------------------
+-- -- Step #1: Accept Anaconda Terms & Conditions
+-- -- ----------------------------------------------------------------------------
 
--- See Getting Started section in Third-Party Packages (https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started)
+-- -- See Getting Started section in Third-Party Packages (https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started)
 
 
--- ----------------------------------------------------------------------------
--- Step #2: Create the account level objects
--- ----------------------------------------------------------------------------
-USE ROLE ACCOUNTADMIN;
+-- -- ----------------------------------------------------------------------------
+-- -- Step #2: Create the account level objects
+-- -- ----------------------------------------------------------------------------
+-- USE ROLE ACCOUNTADMIN;
 
--- Roles
-SET MY_USER = CURRENT_USER();
-CREATE OR REPLACE ROLE HOL_ROLE;
-GRANT ROLE HOL_ROLE TO ROLE SYSADMIN;
-GRANT ROLE HOL_ROLE TO USER IDENTIFIER($MY_USER);
+-- -- Roles
+-- SET MY_USER = CURRENT_USER();
+-- CREATE OR REPLACE ROLE HOL_ROLE;
+-- GRANT ROLE HOL_ROLE TO ROLE SYSADMIN;
+-- GRANT ROLE HOL_ROLE TO USER IDENTIFIER($MY_USER);
 
-GRANT EXECUTE TASK ON ACCOUNT TO ROLE HOL_ROLE;
-GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE HOL_ROLE;
-GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE HOL_ROLE;
+-- GRANT EXECUTE TASK ON ACCOUNT TO ROLE HOL_ROLE;
+-- GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE HOL_ROLE;
+-- GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE HOL_ROLE;
 
--- Databases
-CREATE OR REPLACE DATABASE HOL_DB;
-GRANT OWNERSHIP ON DATABASE HOL_DB TO ROLE HOL_ROLE;
+-- -- Databases
+-- CREATE OR REPLACE DATABASE HOL_DB;
+-- GRANT OWNERSHIP ON DATABASE HOL_DB TO ROLE HOL_ROLE;
 
--- Warehouses
-CREATE OR REPLACE WAREHOUSE HOL_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
-GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
+-- -- Warehouses
+-- CREATE OR REPLACE WAREHOUSE HOL_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
+-- GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
 
 
 -- ----------------------------------------------------------------------------
@@ -42,7 +42,7 @@ GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
 -- ----------------------------------------------------------------------------
 USE ROLE HOL_ROLE;
 USE WAREHOUSE HOL_WH;
-USE DATABASE HOL_DB;
+USE DATABASE HOL_DB97;
 
 -- Schemas
 CREATE OR REPLACE SCHEMA EXTERNAL;
